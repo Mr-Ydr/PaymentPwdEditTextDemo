df018a82ff3ffd00c9e96ce8f42379b5d1f567dd
应东日 <drying@erongdu.com>
2018-11-07 16:53:58 +0800

    1、设置输入完毕回调

--- app/src/main/java/com/drying/ppe/MainActivity.java
+++ app/src/main/java/com/drying/ppe/MainActivity.java
@@ -2,11 +2,21 @@ package com.drying.ppe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private PaymentPwdEditText ppe;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ppe = findViewById(R.id.ppe);
+        ppe.setInputEndClick(new PaymentPwdEditText.InputEndClick() {
+            @Override
+            public void onInputEndClick(String text) {
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
